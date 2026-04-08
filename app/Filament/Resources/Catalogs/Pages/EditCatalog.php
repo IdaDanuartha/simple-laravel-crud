c79b0aef05ce0ae93577d4f0ae7b8a4f9adb04cb
DanDev_ <danuart21@gmail.com>
2026-04-08 13:09:18 +0800

    feat: implement Catalog resource with database migration, model, and Filament CRUD pages

---
+++ app/Filament/Resources/Catalogs/Pages/EditCatalog.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filament\Resources\Catalogs\Pages;
+
+use App\Filament\Resources\Catalogs\CatalogResource;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\ViewAction;
+use Filament\Resources\Pages\EditRecord;
+
+class EditCatalog extends EditRecord
+{
+    protected static string $resource = CatalogResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            ViewAction::make(),
+            DeleteAction::make(),
+        ];
+    }
+}
---
+++ app/Filament/Resources/Catalogs/Schemas/CatalogForm.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Filament\Resources\Catalogs\Schemas;
+
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class CatalogForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('title')
+                    ->required(),
+                RichEditor::make('description')
+                    ->required()
+                    ->columnSpanFull(),
+                TextInput::make('price')
+                    ->required()
+                    ->numeric()
+                    ->prefix('$'),
+                FileUpload::make('image')
+                    ->image(),
+                TextInput::make('category')
+                    ->required(),
+            ]);
+    }
+}
